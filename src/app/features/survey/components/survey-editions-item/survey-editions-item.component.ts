2abe011992683b5391c58b6064cbbafd79f9a950
ahmed <foudiahmed82@gmail.com>
2024-12-26 09:53:46 +0100

    creation des components surveys and survey-edition

---
+++ src/app/features/survey/components/survey-editions-item/survey-editions-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-editions-item',
+  imports: [],
+  templateUrl: './survey-editions-item.component.html',
+  styleUrl: './survey-editions-item.component.css'
+})
+export class SurveyEditionsItemComponent {
+
+}
